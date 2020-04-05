eef9f48365ff5b89c4532f3071e19300518ffc01
shaannessy25 <47993680+shaannessy25@users.noreply.github.com>
2020-04-05 13:02:02 -0700

    initial commit

---
+++ reverse-integer.py
@@ -0,0 +1,22 @@
+# Given a 32-bit signed integer, reverse digits of an integer 
+# Example 1: input = [1,2,3] => output = [3,2,1]
+# Example 2: input = [-1,2,3] => output = [-3,2,1]
+# Example 3: input: 120 => 21 
+# Assume we are dealing with an enviornment which could only store integers
+# within the 32-bit signed integer range: [-2^31, 2^31-1]. For the purpose
+# of this problem, assume That your function returns 0 when the reversed integer
+# overflows
+
+# Restate the question: 
+#   
+# Clarifying questions
+
+# Stating Assumptions
+
+# Brainstorm:
+
+# Explaining rationale:
+
+# Tradeoffs:
+
+# Improvements:
---
+++ roman-to-integer.py
@@ -0,0 +1,28 @@
+# Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M
+# Given a roman numeral, convert it to an integer. Input is guranteed to be within the range
+# from 1 to 3999. 
+#Symbol  Value
+#I       1
+#V       5
+#X       10
+#L       50
+#C       100
+#D       500
+#M       1000
+
+# Example 1: input = III => output = 3
+# Example 2: input = IV => output = 4
+
+# Restate the problem: 
+
+# Clarifying questions
+
+# Stating Assumptions
+
+# Brainstorm:
+
+# Explaining rationale:
+
+# Tradeoffs:
+
+# Improvements:
